19b9768d705dbb603998981a6e54716757aa5ca3
krispy <krishnapandeykp@users.noreply.github.com>
2024-07-01 17:39:51 +0530

    docs

---
+++ KrishnaSpeaksIN/docs/server/chunk-ZPWOHWXO.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a as r,b as i,c as p,d as n,e as m,l as s,n as a,o as f,r as c,x as d}from"./chunk-CDJGTFLA.mjs";var l=(()=>{let o=class o{constructor(){this.title="KrishnaSpeaksIN"}};o.\u0275fac=function(e){return new(e||o)},o.\u0275cmp=r({type:o,selectors:[["app-root"]],standalone:!0,features:[m],decls:2,vars:0,template:function(e,y){e&1&&(i(0,"h1"),n(1,"Hello!"),p())}});let t=o;return t})();var u=[];var v={providers:[d(u),f()]};var x={providers:[c()]},g=s(v,x);var h=()=>a(l,g),O=h;export{O as a};
---
+++ KrishnaSpeaksIN/docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-ZPWOHWXO.mjs";import"./chunk-CDJGTFLA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
